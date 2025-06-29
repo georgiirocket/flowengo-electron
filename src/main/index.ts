7ed8f8c69c20d196befa17902c08dfbca45b2c47
Rocket_GR <georgiiromanishko@gmail.com>
2025-06-29 12:39:33 +0200

    feat(project): app one instance

--- src/main/index.ts
+++ src/main/index.ts
@@ -7,6 +7,8 @@ import { setInvokes } from './invoke'
 import { checkUpdates } from './updater'
 import { appWindow } from './app-window'
 
+const gotTheLock = app.requestSingleInstanceLock()
+
 function createWindow(): void {
   appWindow.newWindow()
 
@@ -31,42 +33,54 @@ function createWindow(): void {
   }
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.whenReady().then(async () => {
-  // Set app user model id for windows
-  electronApp.setAppUserModelId('com.electron')
+// Only one app instance
+if (!gotTheLock) {
+  app.quit()
+} else {
+  app.on('second-instance', () => {
+    const mainWindow = appWindow.getMainWindow()
 
-  // Default open or close DevTools by F12 in development
-  // and ignore CommandOrControl + R in production.
-  // see https://github.com/alex8088/electron-toolkit/tree/master/packages/utils
-  app.on('browser-window-created', (_, window) => {
-    optimizer.watchWindowShortcuts(window)
+    // Someone tried to run a second instance, we should focus our window.
+    if (mainWindow && mainWindow.isMinimized()) {
+      mainWindow.restore()
+      mainWindow.focus()
+    }
   })
 
-  createMenu()
-  setInvokes()
-  await initDatabase()
-  createWindow()
-
-  app.on('activate', function () {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
-    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  // Quit when all windows are closed, except on macOS. There, it's common
+  // for applications and their menu bar to stay active until the user quits
+  // explicitly with Cmd + Q.
+  app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+      app.quit()
+    }
   })
 
-  checkUpdates()
-})
+  // This method will be called when Electron has finished
+  // initialization and is ready to create browser windows.
+  // Some APIs can only be used after this event occurs.
+  app.whenReady().then(async () => {
+    // Set app user model id for windows
+    electronApp.setAppUserModelId('com.electron')
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
-})
+    // Default open or close DevTools by F12 in development
+    // and ignore CommandOrControl + R in production.
+    // see https://github.com/alex8088/electron-toolkit/tree/master/packages/utils
+    app.on('browser-window-created', (_, window) => {
+      optimizer.watchWindowShortcuts(window)
+    })
 
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
+    createMenu()
+    setInvokes()
+    await initDatabase()
+    createWindow()
+
+    app.on('activate', function () {
+      // On macOS it's common to re-create a window in the app when the
+      // dock icon is clicked and there are no other windows open.
+      if (BrowserWindow.getAllWindows().length === 0) createWindow()
+    })
+
+    checkUpdates()
+  })
+}
